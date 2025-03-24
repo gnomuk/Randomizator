99bcc4e0d0b94d451d42337098b3b2d61e9e53d3
gnomuk <desckript.rus@yandex.ru>
2025-03-24 03:25:08 +0300

    Добавьте файлы проекта.

---
+++ Randomizator/Classes/ScriptCollection.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+
+public class ScriptInfo : INotifyPropertyChanged
+{
+    private bool _isChecked;
+
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public bool Enabled { get; set; }
+    public Visibility Show_Gear { get; set; }
+
+    public ScriptInfo(string name, string description, bool enabled, Visibility showGear)
+    {
+        Name = name;
+        Description = description;
+        Enabled = enabled;
+        Show_Gear = showGear;
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string script)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(script));
+    }
+}
+
+public class ScriptEntry
+{
+    public string ShortName { get; set; }
+    public ScriptInfo Info { get; set; }
+
+    public ScriptEntry(string shortName, ScriptInfo info)
+    {
+        ShortName = shortName;
+        Info = info;
+    }
+}
+
+public class ScriptCollection : INotifyPropertyChanged
+{
+    public ObservableCollection<ScriptEntry> Scripts { get; set; }
+
+    public ScriptCollection()
+    {
+        Scripts = new ObservableCollection<ScriptEntry>();
+    }
+
+    public void AddScript(string shortName, string name, string description, bool enabled, Visibility showGear)
+    {
+        var scriptInfo = new ScriptInfo(name, description, enabled, showGear);
+        Scripts.Add(new ScriptEntry(shortName, scriptInfo));
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected virtual void OnPropertyChanged(string script)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(script));
+    }
+}
---
+++ Randomizator/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace Randomizator
+{
+    public partial class MainWindow : Window
+    {
+        List<ScriptEntry> enabledScripts = new List<ScriptEntry>();
+        ScriptEntry selectedScript;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new ScriptCollection();
+        }
+
+        #region Window Handlers
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            var viewModel = (ScriptCollection)this.DataContext;
+            CollectScripts(viewModel);
+            scriptsLabel.Content = $"Список сценариев ({viewModel.Scripts.Count()})";
+        }
+
+        private void DragWindow(object sender, System.Windows.Input.MouseButtonEventArgs e) { DragMove(); }
+
+        private void CloseWindow(object sender, RoutedEventArgs e) { Close(); }
+
+        private void MinimizeWindow(object sender, RoutedEventArgs e) { WindowState = WindowState.Minimized; }
+
+        private void StartScriptRoulette(object sender, RoutedEventArgs e)
+        {
+            //await Task.Run(ScriptRoulette);
+        }
+
+        private void PinWindow(object sender, RoutedEventArgs e)
+        {
+            if (this.Topmost)
+            {
+                this.Topmost = false;
+                PinButton_Image.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/Images/grayPin.png"));
+                return;
+            }
+            this.Topmost = true;
+            PinButton_Image.Source = new BitmapImage(new Uri("pack://application:,,,/Assets/Images/bluePin.png"));
+        }
+        #endregion
+
+        #region Other Handlers
+        private void ApplyChanges(object sender, RoutedEventArgs e)
+        {
+            var viewModel = (ScriptCollection)this.DataContext;
+            enabledScripts = viewModel.Scripts.Where(scriptEntry => scriptEntry.Info.Enabled).ToList();
+        }
+
+        private void ScriptSettings(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            var item = button?.DataContext as ScriptEntry;
+            selectedScript = item;
+            scriptDescription.Text = item.Info.Description;
+        }
+
+        private void KeyboardInput(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Helpers
+        private void CollectScripts(ScriptCollection viewModel)
+        {
+            viewModel.AddScript("Drop", "Выбросить оружие", "Выбрасывает оружие которое вы держите в руках.", true, Visibility.Visible);
+            viewModel.AddScript("Jump", "Прыжок", "Нажимает кнопку \"Прыжка\".", true, Visibility.Visible);
+            viewModel.AddScript("Inspect", "Осмотр оружия", "Запускает анимацию осмотра оружия.", false, Visibility.Visible);
+            viewModel.AddScript("Reload", "Перезарядка", "Перезаряжает оружие которое вы держите в руках.", false, Visibility.Visible);
+            viewModel.AddScript("Fire", "Огонь", "Нажимает кнопку \"Выстрела\".", false, Visibility.Visible);
+            viewModel.AddScript("SFire", "Альтернативный огонь", "Нажимает кнопку \"Альтернативного огня\", обычно используется для прицеливания и переключения режима стрельбы.", false, Visibility.Visible);
+            viewModel.AddScript("Voice", "Голосовой чат", "Включает ваш микрофон на N секунд.", false, Visibility.Visible);
+            viewModel.AddScript("Crouch", "Приседание", "Зажимает кнопку \"Приседания\" на N секунд.", false, Visibility.Visible);
+            viewModel.AddScript("Sneak", "Медленная ходьба", "Зажимает кнопку \"Медленной ходьбы\" на N секунд.", false, Visibility.Visible);
+            viewModel.AddScript("WeaponSwitch", "Переключение оружия", "Переключает оружие в руках на случайное.", false, Visibility.Visible);
+            viewModel.AddScript("WalkingSomewhere", "Случайная ходьба", "Задает направление ходьбы в случайное направление (вперёд, назад, вправо, влево).", false, Visibility.Visible);
+            viewModel.AddScript("TurnAround", "Разворот", "Разворачивает персонажа на N градусов.", false, Visibility.Visible);
+            viewModel.AddScript("Ping", "Метка", "Ставит внутри-игровую метку по направлению взгляда.", false, Visibility.Visible);
+            viewModel.AddScript("DisablingTheKeyboard", "Отключение клавиатуры", "Полностью отключает ввод с клавиатуры на N секунд.", false, Visibility.Visible);
+            viewModel.AddScript("DisablingTheMouse", "Отключение мыши", "Полностью отключает ввод с мыши на N секунд.", false, Visibility.Visible);
+        }
+        #endregion
+
+        #region Async Tasks
+
+        //private async Task ScriptRoulette()
+        //{
+
+        //}
+
+        #endregion
+    }
+}
